b2888a859bcbc89398dc435595b6159c9c012180
aleena127 <2410030127@klh.edu.in>
2025-11-04 21:46:48 +0530

    signup backend commited

--- weather/my-app/src/Signup.js
+++ weather/my-app/src/Signup.js
@@ -11,7 +11,7 @@ function Signup() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/signup", {
+      const response = await fetch("https://weather-api-zk5f.onrender.com/signup", { 
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
