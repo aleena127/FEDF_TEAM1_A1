4fe41440c7f4953b9b106fe835d30fed7f1d5811
aleena127 <2410030127@klh.edu.in>
2025-11-05 07:50:53 +0530

    retracted login to localhost

--- weather/my-app/src/login.js
+++ weather/my-app/src/login.js
@@ -10,10 +10,15 @@ function Login({ onLogin }) {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://weather-api-zk5f.onrender.com/login", {
+
+      const response = await fetch("http://localhost:5000/login", {
+
         method: "POST",
+
         headers: { "Content-Type": "application/json" },
+
         body: JSON.stringify({ username, password }),
+
       });
 
       const data = await response.json();
